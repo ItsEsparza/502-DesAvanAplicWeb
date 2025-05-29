c9904a0a0bb27e6df13d3203d9afe58c219c4991
Nikole <nikole.mrs06@gmail.com>
2025-05-29 16:44:59 -0600

    modified entire vite.config

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -3,18 +3,17 @@ import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
 
-// Para poder usar __dirname en un config ESM/TS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
-  // si tu app se va a servir desde una IP o ruta relativa, esto ayuda a que los assets funcionen
   base: './',
   build: {
     rollupOptions: {
       input: {
-        // tu HTML secundario
-        menu: resolve(__dirname, 'src/class1/A01784521/menu/menu.html'),
+        main: resolve(__dirname, 'index.html'),
+        // fija la ruta correcta aquí:
+        menu: resolve(__dirname, 'src/class1/A01784521/menu/menu.html')
       }
     }
   }
