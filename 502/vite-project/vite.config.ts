829a10f12aa2878d8c2d80289ebfa88d9923591a
Nikole <nikole.mrs06@gmail.com>
2025-05-29 17:46:51 -0600

    5

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -9,10 +9,10 @@ export default defineConfig({
     rollupOptions: {
       input: {
         app: path.resolve(__dirname, 'index.html'),
-        menu: path.resolve(__dirname, 'src/menu/A01784521/index.html'),
+        menu: path.resolve(__dirname, 'src/class1/A01784521/index.html'),
       }
     },
-    outDir: 'dist',
+    outDir: 'dist', // carpeta de salida común
     emptyOutDir: true
   }
 })
