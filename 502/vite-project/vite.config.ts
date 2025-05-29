02ddc781d461d82b868263f02b0491fa0bc43add
Nikole <nikole.mrs06@gmail.com>
2025-05-29 17:39:52 -0600

    4

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -1,22 +1,18 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// 1) Obtenemos __filename y __dirname correctamente
-const __filename = fileURLToPath(import.meta.url)
-const __dirname  = dirname(__filename)
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
-        // Página principal
-        main: resolve(__dirname, 'index.html'),
-        // Tu HTML secundario: apunta al path real dentro de src/
-        menu: resolve(__dirname, 'src/class1/A01784521/index.html')
+        app: path.resolve(__dirname, 'index.html'),
+        menu: path.resolve(__dirname, 'src/menu/A01784521/index.html'),
       }
-    }
+    },
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
